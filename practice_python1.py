2cb3562d754383eec67dc9ad137b066c5cfa28a1
Eva Horalikova <eva.horalikova@gmail.com>
2016-12-28 12:11:32 +0100

    calculates when you will be 100 y.o.

---
+++ practice_python1.py
@@ -0,0 +1,10 @@
+age=int(input("age: "))
+name=input("name: ")
+
+year= 100- age + 2016
+
+if age<100:
+  print ("{}, you will be 100 in the year {}".format(name, year))
+else:
+  print ("{}, you were 100 in the year {}".format(name, year))
+  

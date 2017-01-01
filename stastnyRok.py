fea266c5dcd822906b800899d5c3e2c5ca1b2a57
Eva Horalikova <eva.horalikova@gmail.com>
2017-01-01 19:15:07 +0100

    counts the sum of the digits of two years from input

---
+++ stastnyRok.py
@@ -0,0 +1,30 @@
+year1 = input("from what year? (a four digit number please)")
+year2 = input("to what year? (a four digit number please)")
+digitis1= [int(d) for d in str(year1)]
+print(digitis1)
+digitis2=[int(d) for d in str(year2)]
+print (digitis2)
+
+sum1= sum(digitis1)
+print (sum1)
+sum2= sum(digitis2)
+print (sum2)
+
+digitis1Final= [int(d) for d in str(sum1)]
+digitis2Final= [int(d) for d in str(sum2)]
+sum1Final1=sum(digitis1Final)
+print(sum1Final1)
+sum2Final2=sum(digitis2Final)
+print(sum2Final2)
+
+if sum1Final1==1:
+  print( "{} will/was a lucky year".format(year1))
+
+else:
+  print ("{} will/was unfourtunatly not a lucky year".format(year1))
+
+if sum1Final1==1:
+  print("{} will/was a lucky year".format(year2))
+else:
+  print ("{} will/was unfourtunatly not a lucky year".format(year2))
+  

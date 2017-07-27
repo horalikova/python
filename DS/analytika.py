ada0efda163f12e3b677ed2987358bcd10bf1f90
Eva Horalikova <eva.horalikova@gmail.com>
2017-07-27 10:25:39 +0200

    top 10 words, english only

--- DS/analytika.py
+++ DS/analytika.py
@@ -1,6 +1,25 @@
 import codecs
+import nltk 
+from collections import Counter
 
-path = 'C:\Users\evca\Desktop\DS\sloh.txt'
+from nltk.corpus import stopwords
+stop = set(stopwords.words('english'))
+
+#path = 'C:\Users\evca\Desktop\DS\sloh.txt'
+path = "algebra.txt"
 sloh_file = codecs.open(path,'r',"utf-8")
 content=sloh_file.read()
 print(content)
+
+words=content.split()
+results=[]
+for word in words:
+     if word not in stop:
+        results.append(word)
+print(results)        
+
+
+cntr = Counter(results)
+print(cntr.most_common(10))
+
+

f1b7d49a4350512d0d892eb7b6e7a25cd3e91f46
Eva Horalikova <eva.horalikova@gmail.com>
2017-07-27 19:10:28 +0200

    pozna english/czech

--- DS/analytika.py
+++ DS/analytika.py
@@ -1,18 +1,24 @@
 from nltk.corpus import stopwords
 from collections import Counter
-#path="sloh.txt"
-path="rocnikovka.txt"
+path="sloh.txt"
+#path="rocnikovka.txt"
 file=open(path, "r")
 obsah=file.read().decode("utf-8", "ignore").encode("utf-8")
 #print(obsah)
 
 words=obsah.split()
-#print(words)
 
-#stop=set(stopwords.words("english"))
-file2=open("czechStopwords.txt", "r")
-obsah2=file2.read().decode("utf-8", "ignore").encode("utf-8")
-stop=set(obsah2.split())
+if ("the" in words) and ("and" in words):
+  language="english"
+else:
+  language="czech"
+
+if  language=="english":
+  stop=set(stopwords.words("english"))
+else:
+  file2=open("czechStopwords.txt", "r")
+  obsah2=file2.read().decode("utf-8", "ignore").encode("utf-8")
+  stop=set(obsah2.split())
 #print(stop)
 
 clean=[]

ddaab3b5478558db35c6a8a769158973f3fa9404
evahoralikova <eva.horalikova@gmail.com>
2017-11-15 16:16:39 +0100

    finished, doesn't count how many cycles

--- 6147.py
+++ 6147.py
@@ -3,8 +3,22 @@ n1 = list(map(int,str(n)))
 list.sort(n1)
 num1 = int(''.join(map(str,n1)))
 list.sort(n1, reverse = True)
-print(num1)  
+print(num1)
 num2 = int(''.join(map(str,n1)))
 print(num2)
-sub1 = num2 - num1    
+sub1 = num2 - num1
 print (sub1)
+
+while sub1!=6147:
+  sub=sub1
+  n1 = list(map(int,str(sub)))
+  list.sort(n1)
+  num1 = int(''.join(map(str,n1)))
+  list.sort(n1, reverse = True)
+  print(num1)
+  num2 = int(''.join(map(str,n1)))
+  print(num2)
+  sub1 = num2 - num1
+  print (sub1)
+  if sub1==6147:
+     break
